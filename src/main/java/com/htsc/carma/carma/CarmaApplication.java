e9f7ba4f0e42cd43fe4815d0833e1e8ef4a784bd
DELL <1824938204@qq.com>
2019-12-26 13:24:15 +0800

    测试Aop

---
+++ src/main/java/com/htsc/carma/carma/CarmaApplication.java
@@ -0,0 +1,16 @@
+package com.htsc.carma.carma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DELL
+ */
+@SpringBootApplication
+public class CarmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarmaApplication.class, args);
+    }
+
+}
